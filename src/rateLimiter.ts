6eb4806d46150062b8c2534c6e8e6da9e3d35e84
vengadeshraj <vengadeshraj405@gmail.com>
2025-12-14 15:50:02 +0530

    rate limit data type added

--- src/rateLimiter.ts
+++ src/rateLimiter.ts
@@ -1,8 +1,13 @@
+import { Request, Response, NextFunction } from "express";
 import { MAX_REQUEST, STATUS_CODE, WINDOW_SIZE } from "./constants";
 
 const rateLimitMap = new Map();
 
-export const rateLimiter = (req: any, res: any, next: any) => {
+export const rateLimiter = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   const ip = req.ip;
   const now = Date.now();
 
@@ -12,7 +17,7 @@ export const rateLimiter = (req: any, res: any, next: any) => {
 
   const timestamps = rateLimitMap
     .get(ip)
-    .filter((ts: any) => now - ts < WINDOW_SIZE);
+    .filter((ts: number) => now - ts < WINDOW_SIZE);
   timestamps.push(now);
 
   if (timestamps.length > MAX_REQUEST) {
